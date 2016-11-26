d849990a9bc73a5a4fb58d2f08f4647549253c2f
Nikita Sinyavskiy <Nikita Sinyavskiy>
2016-11-27 01:21:15 +0300

    Check change of code

--- Black_Jack/Black_Jack/Form1.cs
+++ Black_Jack/Black_Jack/Form1.cs
@@ -15,6 +15,7 @@ namespace Black_Jack
         public Form1()
         {
             InitializeComponent();
+
             var pack = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
             var Dealer = [];
             var Player = [];
